ee3d44508e421135da7a3be14c0322b66ea6f44f
Jake Mays <jake.m.mays@gmail.com>
2018-02-07 12:49:12 -0700

    Typo

--- config/prod.exs
+++ config/prod.exs
@@ -66,7 +66,7 @@ config :smart_ac, SmartAc.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true,
+  ssl: true
 
 
 config :smart_ac, SmartAcWeb.Mailer,
